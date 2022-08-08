e094fe6cd2ae72a4e70c07eee1707584eca25ea4
ddmkim94 <ddmkim94@gmail.com>
2022-08-08 13:01:57 +0900

    @ResponseBody 사용!

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -2,12 +2,16 @@ package com.ll.exam.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/sbb")
-    public void index() {
-        System.out.println("index");
+    @ResponseBody
+    // 메서드의 리턴값을 그대로 브라우저에 표시!
+    // 아래 함수의 리턴값을 문자열화 해서 브라우저 응답의 바디에 담는다.
+    public String index() {
+        return "박은빈";
     }
 }
