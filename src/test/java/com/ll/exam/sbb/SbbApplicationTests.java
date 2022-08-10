b8e90395f6cba03c561b4a1b472d45ce67ca569f
ddmkim94 <ddmkim94@gmail.com>
2022-08-10 14:37:44 +0900

    findBySubjectAndContent() 테스트

--- src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
@@ -53,4 +53,11 @@ class SbbApplicationTests {
 		assertEquals(1, q.getId());
 	}
 
+	@Test
+	void findBySubjectAndContentTest() {
+		Question q = this.questionRepository.findBySubjectAndContent(
+				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+		assertEquals(1, q.getId());
+	}
+
 }
