ea8f79a4f1b30c2a5ce6d06c1a5fc7fa89da6bc0
ddmkim94 <ddmkim94@gmail.com>
2022-08-12 11:45:32 +0900

    answer 조회 테스트

--- src/test/java/com/ll/exam/sbb/AnswerRepositoryTest.java
+++ src/test/java/com/ll/exam/sbb/AnswerRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 public class AnswerRepositoryTest {
     @Autowired
@@ -27,13 +29,26 @@ public class AnswerRepositoryTest {
 
     private void createSampleData() {
         QuestionRepositoryTest.createSampleData(questionRepository);
+
         Question q = questionRepository.findById(1).get();
 
-        Answer a = new Answer();
-        a.setContent("네 자동으로 생성됩니다.");
-        a.setQuestion(q);
-        a.setCreateDate(LocalDateTime.now());
-        answerRepository.save(a);
+        Answer a1 = new Answer();
+        a1.setContent("sbb는 질문답변 게시판 입니다.");
+        a1.setQuestion(q);
+        a1.setCreateDate(LocalDateTime.now());
+        answerRepository.save(a1);
+
+        Answer a2 = new Answer();
+        a2.setContent("sbb에서는 주로 스프링부트관련 내용을 다룹니다.");
+        a2.setQuestion(q);
+        a2.setCreateDate(LocalDateTime.now());
+        answerRepository.save(a2);
+    }
+
+    @Test
+    void 조회() {
+        Answer a = this.answerRepository.findById(1).get();
+        assertThat(a.getContent()).isEqualTo("sbb는 질문답변 게시판 입니다.");
     }
 
     @Test
